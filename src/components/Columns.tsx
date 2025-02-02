d75fc17e73bc1f7f8a293f5c159559db4b62b456
sagarmish1234@gmail.com <sagarmish1234@gmail.com>
2025-02-02 22:02:44 +0530

    UI set up completed

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -3,8 +3,16 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Checkbox } from "./ui/checkbox";
-import { ArrowUpDown } from "lucide-react"
+import { ArrowUpDown, Filter } from "lucide-react"
 import { Button } from "./ui/button";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export type Problem = {
   Rating: number;
@@ -53,8 +61,8 @@ export const columns: ColumnDef<Problem>[] = [
         </div>
       )
     },
-    filterFn: (row, id, value: [string, string]) => {
-      const rating = `${Math.round(row.original.Rating)}`;
+    filterFn: (row, _id, value: [string, string]) => {
+      const rating = Math.round(row.original.Rating);
       const [start, end] = value;
       if (start != '' && rating < parseInt(start))
         return false;
@@ -66,9 +74,36 @@ export const columns: ColumnDef<Problem>[] = [
   },
   {
     id: "select",
-    header: "Status",
+    header: ({ column }) => {
+      return (
+        <div className="flex justify-center">
+
+          <Select onValueChange={(value) => {
+            column.setFilterValue(value)
+          }}>
+            <SelectTrigger className="w-32">
+              <SelectValue placeholder="Status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All</SelectItem>
+              <SelectItem value="true">completed</SelectItem>
+              <SelectItem value="false">pending</SelectItem>
+            </SelectContent>
+          </Select>
+          {/* <Button */}
+          {/*   variant="ghost" */}
+          {/*   onClick={() => { */}
+          {/*     column.setFilterValue() */}
+          {/*   }} */}
+          {/* > */}
+          {/*   Rating */}
+          {/*   <Filter className="ml-2 h-4 w-4" /> */}
+          {/* </Button> */}
+        </div>
+      )
+    },
     cell: ({ row }) => (
-      <div className="flex justify-start">
+      <div className="flex justify-center">
         <Checkbox
           checked={row.getIsSelected()}
           onCheckedChange={(value) => row.toggleSelected(!!value)}
@@ -76,6 +111,13 @@ export const columns: ColumnDef<Problem>[] = [
         />
       </div>
     ),
+    filterFn: (row, _id, value: string) => {
+      if (value == "all")
+        return true;
+      const isSelected = row.getIsSelected();
+      const result = value === 'true'
+      return result == isSelected;
+    },
     size: 50,
     enableSorting: false,
     enableHiding: false,
